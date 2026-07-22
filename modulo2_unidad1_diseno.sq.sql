b922f89f271b15b18fefa5b9979a8fe0dbfe5188
Jazmín Avila <avilacelestej@gmail.com>
2026-07-22 20:41:14 -0300

    Update modulo2_unidad1_diseno.sq.sql

--- modulo2_unidad1_diseno.sq.sql
+++ modulo2_unidad1_diseno.sq.sql
@@ -1,38 +1,37 @@
 CREATE DATABASE PracticasSQL;
-USE PracticasSQL;
 
 -- Tabla de clientes
-CREATE TABLE Clientes (
-ID_Cliente INT PRIMARY KEY,
-Nombre varchar(100) NOT NULL,
-Perfil_Bio TEXT,
-Fecha_Registro DATE NOT NULL
+CREATE TABLE clientes (
+id_cliente INT PRIMARY KEY,
+nombre varchar(100) NOT NULL,
+perfil_bio TEXT,
+fecha_registro DATE NOT NULL
 );
 
 --Tabla de productos
-CREATE TABLE Productos (
-ID_Producto INT PRIMARY KEY,
-Descripcion VARCHAR(255) NOT NULL,
-Precio DECIMAL(10,2) NOT NULL,
-Esta_activo VARCHAR(10) NOT NULL
+CREATE TABLE productos (
+id_producto INT PRIMARY KEY,
+descripcion VARCHAR(255) NOT NULL,
+precio DECIMAL(10,2) NOT NULL,
+esta_activo VARCHAR(10) NOT NULL
 );
 
 --Tabla de clientes:
 --INT porque el identificador de cada cliente será un número entero único.
--- ID_Cliente INT PRIMARY KEY
+-- id_cliente INT PRIMARY KEY
 --VARCHAR(100) porque el nombre es un texto de longitud variable y no superará los 100 carácteres.
 -- nombre VARCHAR (100) NOT NULL
 --TEXT porque permite almacenar una biografía o notas de longitud variable.
--- Perfil_Bio Text
+-- perfil_bio Text
 --DATE porque solo se necesita registrar la fecha de alta del cliente.
--- Fecha_Registro DATE NOT NULL
+-- fecha_registro DATE NOT NULL
 
 --Tabla de productos:
 --INT porque identifica de forma única a cada producto.
--- ID_Producto INT Primary KEY
+-- id_producto INT Primary KEY
 --VARCHAR(255) porque la descrpción es un texto de longitud variable.
--- Descripción VARCHAR(255) NOT NULL
+-- descripción VARCHAR(255) NOT NULL
 --DECIMAL(10,2) porque el precio representa dinero y requiere dos decimales.
--- Precio decimal(10,2) NOT NULL
+-- precio decimal(10,2) NOT NULL
 --VARCHAR(10) porque permite indicar si el producto está "Activo" o Inactivo".
--- Esta_activo VARCHAR(10) NOT NULL
+-- esta_activo VARCHAR(10) NOT NULL
